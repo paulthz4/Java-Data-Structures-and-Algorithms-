507b6a13f904b6b37bfc5ecc11d83051af3a05d6
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-02-25 11:44:18 -0700

    completed implementation of MyArrayList

--- DS and algorithms/Ch24/MyArrayList.java
+++ DS and algorithms/Ch24/MyArrayList.java
@@ -6,91 +6,151 @@ public class MyArrayList<E> implements MyList<E> {
 	static final int INITIAL_CAPACITY = 16;
 	private E[] data = (E[]) new Object[INITIAL_CAPACITY];
 	private int size = 0;
-	
-	public MyArrayList() {	
+
+	public MyArrayList() {
 	}
-	
+
 	public MyArrayList(E[] objects) {
-		for(int i=0; i<objects.length; i++) {
+		for (int i = 0; i < objects.length; i++) {
 			add(objects[i]);
 		}
 	}
-	
+
 	@Override
 	public void add(int index, E e) {
-		// TODO Auto-generated method stub
-		if(index < 0 || index >= size)
-			throw new IndexOutOfBoundsException("index: "+index+", Size: "+size);
-		
+		if (index < 0 || index >= size)
+			throw new IndexOutOfBoundsException("index: " + index + ", Size: " + size);
+
 		ensureCapacity();
-		
-		for(int i=index; i<size;i++) {
-			data[i+1] = data[i];
+
+		for (int i = index; i < size; i++) {
+			data[i + 1] = data[i];
 		}
 		data[index] = e;
 		size++;
 	}
-	
+
 	public void ensureCapacity() {
-		if(size >= data.length) {
-			E[] newData = (E[])(new Object[size * 2 + 1]);
+		if (size >= data.length) {
+			E[] newData = (E[]) (new Object[size * 2 + 1]);
 			System.arraycopy(data, 0, newData, 0, size);
 			data = newData;
 		}
 	}
-	
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return size;
-	}
 
 	@Override
-	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
+	public boolean contains(Object e) {
+		for (int i = 0; i < data.length; i++) {
+			if (e.equals(data[i]))
+				return true;
+		}
 		return false;
 	}
 
 	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+	public E get(int index) {
+		checkIndex(index);
+		return data[index];
 	}
+
+	private void checkIndex(int index) {
+		if (index >= data.length || index < 0)
+			throw new IndexOutOfBoundsException("index: " + index + ", Size: " + size);
+
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	public void trimToSize() {
+	    if (size != data.length) { 
+	      E[] newData = (E[])(new Object[size]);
+	      System.arraycopy(data, 0, newData, 0, size);
+	      data = newData;
+	    } // If size == capacity, no need to trim
+	  }
+
+	  @Override /** Override iterator() defined in Iterable */
+	  public java.util.Iterator<E> iterator() {
+	    return new ArrayListIterator();
+	  }
+	 
+	  private class ArrayListIterator 
+	      implements java.util.Iterator<E> {
+	    private int current = 0; // Current index 
+
+	    @Override
+	    public boolean hasNext() {
+	      return current < size;
+	    }
+
+	    @Override
+	    public E next() {
+	      return data[current++];
+	    }
+
+	    @Override // Remove the element returned by the last next()
+	    public void remove() {
+	    	if (current == 0) // next() has not been called yet
+	        throw new IllegalStateException(); 
+	      MyArrayList.this.remove(--current);
+	    }
+	  }
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public E get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		data = (E[])new Object[INITIAL_CAPACITY];
+	    size = 0;
 	}
 
 	@Override
 	public int indexOf(Object e) {
-		// TODO Auto-generated method stub
-		return 0;
+		for (int i = 0; i < data.length; i++) {
+			if (e.equals(data[i]))
+				return i;
+		}
+		return -1;
 	}
 
 	@Override
 	public int lastIndexOf(E e) {
-		// TODO Auto-generated method stub
-		return 0;
+		for (int i = size - 1; i >= 0; i--) {
+			if (e.equals(data[i]))
+				return i;
+		}
+		return -1;
 	}
 
 	@Override
 	public E remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		checkIndex(index);
+		E e = data[index];
+		for (int i = index; i < size; i++) {
+			data[i] = data[i+1];
+		}
+		data[size-1] = null;
+		size--;
+		return e;
 	}
 
 	@Override
 	public E set(int index, E e) {
-		// TODO Auto-generated method stub
-		return null;
+		checkIndex(index);
+		E old = data[index];
+		data[index] = e;
+		return old;
 	}
-
+	
+	@Override 
+	public String toString() {
+		StringBuilder result = new StringBuilder("[");
+		for(int i=0; i<size;i++) {
+			result.append(data[i]);
+			if(i <size-1) 
+				result.append(", ");
+		}
+		return result.toString()+"]";
+	}
 }
