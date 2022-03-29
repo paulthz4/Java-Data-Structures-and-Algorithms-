19de427e97f9c16caf560cd53cf6099a61fd070c
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-03-29 10:59:33 -0600

    findDuplicate int using HashMap

--- DS and algorithms/String_Manipulation/FindDuplicate.java
+++ DS and algorithms/String_Manipulation/FindDuplicate.java
@@ -1,6 +1,8 @@
 package String_Manipulation;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FindDuplicate {
 
@@ -11,15 +13,28 @@ public class FindDuplicate {
 		System.out.println(findDuplicate(arr));
 		int[] arr2 = {3,1,3,4,2};
 		System.out.println(Arrays.toString(arr2));
-		System.out.println(findDuplicate(arr2));
+		System.out.println(findDuplicateMap(arr2));
 	}
 
+	public static int findDuplicateMap(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for(int i=0; i<nums.length;i++) {
+			if(map.containsKey(nums[i])) {
+				map.put(nums[i], map.get(nums[i])+1);
+				return nums[i];
+			}
+			map.put(nums[i], 1);
+		}
+		return -1;
+	}
+	
 	public static int findDuplicate(int[] nums) {
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = 0; j < nums.length; j++) {
-				if (i != j)
-					if (nums[i] == nums[j])
-						return nums[j];
+		for(int i=0; i<nums.length;i++) {
+			for(int j=0;j<nums.length;j++) {
+				if( i != j) {
+					if(nums[i] == nums[j])
+						return nums[i];
+				}
 			}
 		}
 		return -1;
